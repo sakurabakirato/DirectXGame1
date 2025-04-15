2f582dfcb6da655fd0818f1e9a85e83891d0d4b0
sakurabakirato <k024g0054@g.neec.ac.jp>
2025-04-15 10:31:29 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <Windows.h>
+
+//windowsアプリでのエントリーポイント
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+	//出力ウィンドウへの文字出力
+	OutputDebugStringA("Hello,DirectX!\n");
+
+	return 0;
+
+}
