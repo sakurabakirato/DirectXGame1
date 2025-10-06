61a31f7b4e6ce8f6bb82cc0c24175e746ae30972
煌士 櫻庭 <kira.sakura@outlook.jp>
2025-10-06 14:50:52 +0900

    GE3_01_01

--- main.cpp
+++ main.cpp
@@ -104,7 +104,7 @@ enum BlendMode
 
 // 単位行列
 Matrix4x4 MakeIdentity4x4() {
-	Matrix4x4 identity;
+	Matrix4x4 identity{};
 	identity.m[0][0] = 1.0f;	identity.m[0][1] = 0.0f;	identity.m[0][2] = 0.0f;	identity.m[0][3] = 0.0f;
 	identity.m[1][0] = 0.0f;	identity.m[1][1] = 1.0f;	identity.m[1][2] = 0.0f;	identity.m[1][3] = 0.0f;
 	identity.m[2][0] = 0.0f;	identity.m[2][1] = 0.0f;	identity.m[2][2] = 1.0f;	identity.m[2][3] = 0.0f;
@@ -114,7 +114,7 @@ Matrix4x4 MakeIdentity4x4() {
 
 // 4x4の掛け算
 Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2) {
-	Matrix4x4 result;
+	Matrix4x4 result{};
 	result.m[0][0] = m1.m[0][0] * m2.m[0][0] + m1.m[0][1] * m2.m[1][0] + m1.m[0][2] * m2.m[2][0] + m1.m[0][3] * m2.m[3][0];
 	result.m[0][1] = m1.m[0][0] * m2.m[0][1] + m1.m[0][1] * m2.m[1][1] + m1.m[0][2] * m2.m[2][1] + m1.m[0][3] * m2.m[3][1];
 	result.m[0][2] = m1.m[0][0] * m2.m[0][2] + m1.m[0][1] * m2.m[1][2] + m1.m[0][2] * m2.m[2][2] + m1.m[0][3] * m2.m[3][2];
@@ -234,7 +234,7 @@ Matrix4x4 Inverse(const Matrix4x4& m) {
 		+ m.m[0][2] * m.m[1][1] * m.m[2][3] * m.m[3][0]
 		+ m.m[0][1] * m.m[1][3] * m.m[2][2] * m.m[3][0];
 
-	Matrix4x4 result;
+	Matrix4x4 result{};
 	float recpDeterminant = 1.0f / determinant;
 	result.m[0][0] = (m.m[1][1] * m.m[2][2] * m.m[3][3] + m.m[1][2] * m.m[2][3] * m.m[3][1] +
 		m.m[1][3] * m.m[2][1] * m.m[3][2] - m.m[1][3] * m.m[2][2] * m.m[3][1] -
@@ -304,7 +304,7 @@ Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float botto
 MaterialData LoadMaterialTemplateFile(const std::string& directoryPath, const std::string& filename)
 {
 	//1.中で必要となる変数の宣言
-	MaterialData materialData;//構築するMaterialData
+	MaterialData materialData{};//構築するMaterialData
 	std::string line;//ファイルから読んだ1行を格納するもの
 
 	//2.ファイルを開く
@@ -337,7 +337,7 @@ MaterialData LoadMaterialTemplateFile(const std::string& directoryPath, const st
 ModelData LoadObjFile(const std::string& directoryPath, const std::string& filename) 
 {
 	//1.中で必要となる変数の宣言
-	ModelData modelData;//構築するModelData
+	ModelData modelData{};//構築するModelData
 	std::vector<Vector4> positions;//位置
 	std::vector<Vector3>normals;//法線
 	std::vector<Vector2>texcoords;//テクスチャ座標
@@ -356,26 +356,26 @@ ModelData LoadObjFile(const std::string& directoryPath, const std::string& filen
 
 		if (identifier == "v")
 		{
-			Vector4 position;
+			Vector4 position{};
 			s >> position.x >> position.y >> position.z;
 			position.w = 1.0f;
 			positions.push_back(position);
 		} 
 		else if (identifier == "vt")
 		{
-			Vector2 texcoord;
+			Vector2 texcoord{};
 			s >> texcoord.x >> texcoord.y;
 			texcoords.push_back(texcoord);
 		} 
 		else if (identifier == "vn")
 		{
-			Vector3 normal;
+			Vector3 normal{};
 			s >> normal.x >> normal.y >> normal.z;
 			normals.push_back(normal);
 		}
 		else if (identifier == "f") 
 		{
-			VertexData triangle[3];
+			VertexData triangle[3] = {};
 			//面は三角形限定。その他は未対応
 			for (int32_t faceVertex = 0; faceVertex < 3; faceVertex++)
 			{
@@ -383,7 +383,7 @@ ModelData LoadObjFile(const std::string& directoryPath, const std::string& filen
 				s >> vertexDefinition;
 				//頂点の要素へのIndexは「位置/UV/法線」で格納されているので分解してIndexを取得する
 				std::istringstream v(vertexDefinition);
-				uint32_t elementIndices[3];
+				uint32_t elementIndices[3] = {};
 				for (int32_t element = 0; element < 3; element++)
 				{
 					std::string index;
@@ -564,7 +564,7 @@ IDxcBlob* CompileShader(
 	IDxcBlobEncoding* shaderSource = nullptr;
 	HRESULT hr = dxcUtils->LoadFile(filePath.c_str(), nullptr, &shaderSource);
 	assert(SUCCEEDED(hr));
-	DxcBuffer shaderSourceBuffer;
+	DxcBuffer shaderSourceBuffer{};
 	shaderSourceBuffer.Ptr = shaderSource->GetBufferPointer();
 	shaderSourceBuffer.Size = shaderSource->GetBufferSize();
 	shaderSourceBuffer.Encoding = DXC_CP_UTF8;
@@ -820,8 +820,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	hr = device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&commandAllocator));
 	assert(SUCCEEDED(hr));
 
-	//コマンドリストの生成
 	ID3D12GraphicsCommandList* commandList = nullptr;
+	if(commandAllocator != nullptr)
+	//コマンドリストの生成
 	hr = device->CreateCommandList(0, D3D12_COMMAND_LIST_TYPE_DIRECT, commandAllocator, nullptr, IID_PPV_ARGS(&commandList));
 	assert(SUCCEEDED(hr));
 
@@ -859,7 +860,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//ディスクリプタの先頭を取得
 	D3D12_CPU_DESCRIPTOR_HANDLE rtvStartHandle = rtvDescriptorHeap->GetCPUDescriptorHandleForHeapStart();
 	//RTVを2つ作るのでディスクリプタを２つ用意
-	D3D12_CPU_DESCRIPTOR_HANDLE rtvHandles[2];
+	D3D12_CPU_DESCRIPTOR_HANDLE rtvHandles[2] = {};
 	//１つ目
 	rtvHandles[0] = rtvStartHandle;
 	device->CreateRenderTargetView(swapChainResources[0], &rtvDesc, rtvHandles[0]);
